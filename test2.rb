e93160ddc58e837a458a71ed2794b2b8844bcbc1
Aditya Balwani <adibalwani@gmail.com>
2014-04-06 04:50:30 -0400

    Reintialize

--- test2.rb
+++ test2.rb
@@ -5,35 +5,15 @@ require 'sinatra'
 require 'yelpster'
 require 'json'
 get '/sms-quickstart' do
-  #client = Yelp::Client.new
-#include Yelp::V2::Search::Request
-#Yelp.configure(:yws_id          => 'jIW1niVeaxfQ-GDKDsjeeg',
- #              :consumer_key    => 'xoBfuWAfta2vogYhMsgNkg',
-  #             :consumer_secret => 'SzqKgJTN9VMrxmaBuzprD_psdPI',
-   #            :token           => 'EaX1frcRQknqAOviejeu1dRgBZIha1cp',
-    #           :token_secret    => 'hTPpgXt0ELV21x-gmEkSWLtv0r8')
-
-
   sender = params[:From]
-  account_sid = 'AC4d165f7a1f552d6265f691a7bcfb0204'
-  auth_token = '16294e680544dd2c8e232a551d582aab'
-  #@client = Twilio::REST::Client.new account_sid, auth_token
- 
- 
-  #name = friends[sender] || "Mobile Monkey"
+  friends = {
+    "+14153334444" => "Curious George",
+    "+14158157775" => "Boots",
+    "+14155551234" => "Virgil"
+  }
+  name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    #request = Location.new(
-     #        :term => @client.account.sms.messages.list.first.body.to_s[6..@client.account.sms.messages.list.first.body.to_s.length],
-      #       :address => @client.account.sms.messages.list.first.body[0..5].to_s,
-       #      :limit => 1)
-    #response = client.search(request)
-    #retards =  response.to_s.split('"')
-    #for i in 0..retards.length
-     # if retards[i]=="name"
-      #  r.Message retards[i+2]
-      #end
-    #end
-    r.Message @client.account.sms.messages.list.first.body
+    r.Message "Hello, #{name}. Thanks for the message."
   end
   twiml.text
 end
